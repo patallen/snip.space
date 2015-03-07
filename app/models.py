50db42ee9bb8a8c6ca4508ef8ee6533a7913189f
Patrick Allen <prallen90@gmail.com>
2015-03-07 02:30:48 -0500

    Implement password hashing and User model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from app import db
 import datetime
-from hashids import Hashids
+from sqlalchemy.ext.hybrid import hybrid_property
+import bcrypt
 
-hashids = Hashids(salt="I love 3 women")
 
 class Snippet(db.Model):
     __tablename__ = 'snippet'
@@ -10,7 +10,7 @@ class Snippet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     body = db.Column(db.Text(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     date_added = db.Column(db.DateTime)
 
     def __init__(self, title, body):
@@ -18,9 +18,6 @@ class Snippet(db.Model):
         self.body = body
         self.date_added = datetime.datetime.utcnow()
 
-    def get_uuid(self):
-        hash_id = hashids.encode(self.id)
-        return hash_id
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -29,7 +26,24 @@ class User(db.Model):
     username = db.Column(db.String(30), unique=True)
     email = db.Column(db.String(60), unique=True)
     first_name = db.Column(db.String(30))
-    password_salt = db.Column(db.String)
-    password_hash = db.Column(db.String)
+    _password_salt = db.Column(db.String)
+    _password_hash = db.Column(db.String)
 
-    def __init__(self, username, email, password):
+    @hybrid_property
+    def password(self):
+        return self._password_hash
+
+    @password.setter
+    def _set_pass(self, password):
+        self._password_salt = bcrypt.gensalt()
+        self._password_hash = bcrypt.hashpw(password, self._password_salt)
+
+    def validate_pass(self, password):
+        return bcrypt.hashpw(password.encode('utf-8'),
+                             self._password_hash) == self._password_hash
+
+    def __init__(self, username, email, first_name, password):
+        self.password = password.encode('utf-8')
+        self.username = username
+        self.email = email
+        self.first_name = first_name
