996e324a89e59d5eb5ab5a49fa5bbe70f14de022
Patrick Allen <prallen90@gmail.com>
2015-06-16 14:32:01 -0400

    Refactor populateChoiceField to sort languages

--- app/util/helpers.py
+++ app/util/helpers.py
@@ -4,4 +4,12 @@ def populateChoiceField(form):
     """Populate the snippet form's language choicefield
     with languages from the database."""
     languages = [(lang.id, lang.display_text) for lang in Language.query.all()]
+
+    # Sort languages and put Text first
+    languages.sort()
+    for i, lang in enumerate(languages):
+        if lang[0] == 'text':
+            text = languages.pop(i)
+            break
+    languages.insert(0, text)
     form.language.choices = languages
