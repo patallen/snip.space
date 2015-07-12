973ea79ab0bed015e78bee325e994503eb75ca24
Patrick Allen <prallen90@gmail.com>
2015-07-12 00:52:23 -0400

    Add url_for_page function for pagination macro
    This helper takes in a request, and a page number,
    merges the url_args with the view_args, and updates the
    page number. This let's the user keep sorting and ordering
    while navigating through pagination

--- app/util/helpers.py
+++ app/util/helpers.py
@@ -1,4 +1,6 @@
 from app.models import Language
+from flask import url_for
+from app import app
 
 
 def populate_choice_field(form, current_user=None):
@@ -15,3 +17,21 @@ def populate_choice_field(form, current_user=None):
             languages.insert(0, text)
             break
     form.language.choices = languages
+
+
+def url_for_page(request, page):
+    """
+    Wrapper for url_for function that merges the view_args with the
+    url_args, updates the page number, and returns the resulting URL.
+
+    This allows for users to navigate through pagination without losing
+    sorting and ordering in the querystring.
+    """
+    view_args = request.view_args.copy()
+    url_args = request.args.to_dict()
+    args = view_args.copy()
+    args.update(url_args)
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+app.jinja_env.globals['url_for_page'] = url_for_page
+    
